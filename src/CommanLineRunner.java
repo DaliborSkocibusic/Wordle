96b0158ca410151a09ff1602556c7aafc046b992
Dalibor Skocibusic <skody001@mymail.unisa.edu.au>
2022-08-28 21:54:35 +0930

    Pretty much done

--- src/CommanLineRunner.java
+++ src/CommanLineRunner.java
@@ -50,8 +50,30 @@ public class CommanLineRunner {
 		
 		
 		while (runner.gameQuit == 'Y' || runner.gameQuit == 'y') {
+			int counter = 6;
+			
 			runner.playOneGame();
+			//boolean didYouWin = false;
+//			while (!didYouWin && counter > 0) {
+//				didYouWin = runner.playOneGame();
+//				System.out.println("Sorry thats not correct. You have: " + counter + "Tries left" );
+//				
+//				System.out.println("Please enter your guess word (5 Letters)");
+//				runner.guessedWord = runner.sc.next();
+//				
+//				while (!validateLegalGuess(runner.guessedWord)) {
+//					System.out.println("Please follow the input rules");
+//					runner.guessedWord = runner.sc.next();
+//				}
+//				counter++;
+//			}
+			
 			runner.gameQuit = runner.playAnotherGame(runner);
+			
+			if (runner.gameQuit == 'Y' || runner.gameQuit == 'y') {
+				runner.chosenWord = runner.chooseWord();
+				System.out.println("The new word is: " + runner.chosenWord);
+			}
 
 		}
 		System.out.println("You choose " + runner.gameQuit + " The game has finished.");
@@ -82,7 +104,7 @@ public class CommanLineRunner {
 		return sc.next().charAt(0);
 	}
 	
-	public void playOneGame() {
+	public boolean playOneGame() {
 		boolean didYouWin = false;
 		System.out.println("Playing one game");
 		System.out.println("Please enter your guess word (5 Letters)");
@@ -96,10 +118,12 @@ public class CommanLineRunner {
 			System.out.println("Congrats you won!");
 		}
 		else {
-			System.out.println("Sorry you lost. The word was: " + this.chosenWord);
+			System.out.println("Sorry that is not correct. The word was: " + this.chosenWord);
 		}
 		
-		System.out.println("The word caugth was: " + this.guessedWord);
+		//System.out.println("The word caugth was: " + this.guessedWord);
+		
+		return didYouWin;
 	}
 	
 	public char playAnotherGame(CommanLineRunner currentGame) {
@@ -107,7 +131,7 @@ public class CommanLineRunner {
 		return currentGame.sc.next().charAt(0);
 	}
 	
-	public boolean validateLegalGuess(String guess) {
+	public static boolean validateLegalGuess(String guess) {
 		if (guess.length() != 5) return false;
 		if (guess.matches("[^A-Za-z]")) return false;
 		
@@ -121,8 +145,10 @@ public class CommanLineRunner {
 		return false;
 	}
 	
-	public char[] getFeedbackOnGuess() {
-		char[] correctGuesses = {' ',' ',' ',' ',' '};
+	public String getFeedbackOnGuess() {
+		char[] correctGuessesCorrectSpot = {' ',' ',' ',' ',' '};
+		char[] correctGuessIncorrectSpot = {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+		int correctGuessIncorrectSpotIndex = 0;
 //		char searchInput0 = this.guessedWord.charAt(0);
 //		char searchInput1 = this.guessedWord.charAt(1);
 //		char searchInput2 = this.guessedWord.charAt(2);
@@ -135,32 +161,68 @@ public class CommanLineRunner {
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
 				if (this.chosenWord.charAt(i) == this.guessedWord.charAt(j)) {
-					correctGuesses[i] = this.chosenWord.charAt(i);
+					if (i == j) {
+						correctGuessesCorrectSpot[i] = this.chosenWord.charAt(i);
+					}
+					else {
+						correctGuessIncorrectSpot[correctGuessIncorrectSpotIndex]=
+								this.guessedWord.charAt(j);
+						correctGuessIncorrectSpotIndex++;
+					}
 				}
 			}	
 		}
 		//System.out.println("Your feed back is: " + Arrays.toString(correctGuesses));
-		return correctGuesses;
+		//String retCorrectGCS = new String(correctGuessesCorrectSpot);
+		//System.out.println("HERE" + retCorrectGCS);
+		String retCorrctGIC = new String(correctGuessIncorrectSpot);
+		
+		String retUniqueIncorrect = "";
+
+		//System.out.println("HERE2" + retCorrctGIC);
+		return  "The guesses in the correct spot are: \t" + Arrays.toString(correctGuessesCorrectSpot) + "\n" +
+				"The guesses in the incorrect spot are: \t" + retCorrctGIC;
+
 	}
 	
 	public boolean enterGuessingLoop() {
+		int noOfGuesses = 6;
+		boolean didYouWin = false;
+		
+		didYouWin = doSingleGuess();
+		while ((noOfGuesses > 0) || didYouWin) {
+			noOfGuesses--;
+			if(didYouWin) return true;
+			else {
+				System.out.println("Sorry you didnt win: Number of tries left: " + noOfGuesses);
+				
+				System.out.println("Please enter your guess word (5 Letters)");
+				this.guessedWord = this.sc.next();
+				while (!validateLegalGuess(guessedWord)) {
+					System.out.println("Please follow the input rules");
+					this.guessedWord = this.sc.next();
+				}
+				
+				didYouWin = doSingleGuess();
+			}
+		}
 		return doSingleGuess();
 	}
 	
 	public boolean doSingleGuess() {
 		boolean guessedResult = false;
-		char[] guessedFeedback = {' ',' ',' ',' ',' '};
+		String guessedFeedback = "";
 		guessedResult = compareGuessToStoredWord();
 		if (guessedResult) {
 			System.out.println("You guessed correctly");
 			return true;
 		}
 		else {
-			System.out.println("You guessed incorrectly. Below is the feedback");
+			System.out.println("You guessed incorrectly. Below is the feedback\n");
 			guessedFeedback = getFeedbackOnGuess();
 
 		}
-		System.out.println("Your feed back is: " + Arrays.toString(guessedFeedback));
+		System.out.println("Your feed back is: \n" + guessedFeedback + "\n");
 		
 		return false;
 	}
